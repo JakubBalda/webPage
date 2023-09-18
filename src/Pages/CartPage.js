d552d5a6b08621a3b46ec1bc579fe857838c58bf
Jakub Balda <balda.jakub00@gmail.com>
2023-09-18 16:18:31 +0200

    Created Cart page

---
+++ src/Pages/CartPage.js
@@ -0,0 +1,14 @@
+import { ContentWrapper, PageWrapper } from "reacthalfmoon";
+import NavbarComponent from "../Components/Navbar";
+import Sidebar from "../Components/Sidebar";
+
+export default function CartPage({cookies}){
+
+    return(
+        <div>
+            <PageWrapper>
+                
+            </PageWrapper>
+        </div>
+    )
+}
